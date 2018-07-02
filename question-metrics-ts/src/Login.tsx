2fd28d66fea69ed4e1bb25f531ee8a4685e7d565
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2018-07-02 07:54:03 -0300

    Criado esqueleto de tela de login

--- question-metrics-ts/src/Login.tsx
+++ question-metrics-ts/src/Login.tsx
@@ -10,13 +10,20 @@ export class Login extends React.Component {
 
     public render() {
       return (        
-        <div>
-            <label>Login</label>
-            <input type="text" />
-            <label>Password</label>
-            <input type="password" />
-            <br />
-            <input type="button" value="Login" />
+        <div className="container">
+            <div className="login-container">
+                    <div id="output">&nbsp</div>
+                    <div className="avatar" />
+                    <p className="appName">Question Metrics</p>
+                    <div className="form-box">
+                        <form action="" method="">
+                            <input name="user" type="text" placeholder="username" />
+                            <input type="password" placeholder="password" />
+                            <button className="btn btn-info btn-block login" type="button">Login</button>
+                        </form>
+                    </div>
+                </div>
+                
         </div>
       )
     }
