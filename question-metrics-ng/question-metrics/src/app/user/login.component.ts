109522dcb0ccc86368bdb8045b390c409adc6d9e
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2018-08-28 08:54:34 -0300

    Ajustado tela de login, instalado NgRx sotre

--- question-metrics-ng/question-metrics/src/app/user/login.component.ts
+++ question-metrics-ng/question-metrics/src/app/user/login.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  title = 'question-metrics Login';
+export class LoginComponent implements OnInit {
+
+  constructor(private router: Router) {
+
+  }
+
+  ngOnInit() {
+    // If Already Loged In
+    if (true) {
+      this.router.navigate(['/dashboard']);
+    }
+  }
+
 }
