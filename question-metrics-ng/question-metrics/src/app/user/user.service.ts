18b24eb60426dbb08693b207e000b228b39bc86a
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2018-09-02 11:40:05 -0300

    Adicionado VM par alogin de usuário, e implementado comportamento em service para login de usuario

--- question-metrics-ng/question-metrics/src/app/user/user.service.ts
+++ question-metrics-ng/question-metrics/src/app/user/user.service.ts
@@ -1,16 +1,42 @@
 import { Injectable } from '@angular/core';
-import { User } from './user';
-import { HttpClient } from 'selenium-webdriver/http';
+import { User, LoginInfo } from './user';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { throwError, Observable } from 'rxjs';
+import { catchError, tap, map } from 'rxjs/operators';
 
 @Injectable()
 export class UserService {
+
+  private userApiUrl = 'user/login';
+
   constructor(private http: HttpClient) {}
 
   getLoggedInUser(): User {
     return JSON.parse(localStorage.getItem('loggedInUser')) as User;
   }
 
-  LogIn(): User {
-    return JSON.parse(localStorage.getItem('loggedInUser')) as User;
+  LogIn(loginInfo: LoginInfo): Observable<User> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post<User>(this.userApiUrl, loginInfo, {headers: headers})
+    .pipe(
+      tap(data => console.log(JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(err) {
+    // in a real world app, we may send the server to some remote logging infrastructure
+    // instead of just logging it to the console
+    let errorMessage: string;
+    if (err.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      errorMessage = `Backend returned code ${err.status}: ${err.body.error}`;
+    }
+    console.error(err);
+    return throwError(errorMessage);
   }
 }
--- question-metrics-ng/question-metrics/src/app/user/user.ts
+++ question-metrics-ng/question-metrics/src/app/user/user.ts
@@ -2,3 +2,8 @@ export interface User {
     id: number;
     userName: string;
 }
+
+export interface LoginInfo {
+    username: string;
+    password: string;
+}
